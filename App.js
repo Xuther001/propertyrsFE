272c8338ac58ce4b1b365335a764623c302c3e1a
Xuther001 <xuther@yahoo.com>
2024-10-12 02:27:27 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Register';
+import PropertyListing from './components/PropertyListing';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
